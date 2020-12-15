55699afe779194d56c7c40f103d28fc04a9fb029
Your Name <lrymchl@gmail.com>
2020-12-15 20:11:53 +0700

    change login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,6 +8,7 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
+                <p></p>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
